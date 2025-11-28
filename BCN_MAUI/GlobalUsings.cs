b6aa10553935f7d00e32d23d35e47d7503917053
NELSONPanus123 <nelson@panus.cm>
2025-11-28 18:15:47 +0100

    MarketPrice Landing-screen

--- BCN_MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using BCN_MAUI.Data;
-global using BCN_MAUI.PageModels;
-global using BCN_MAUI.Pages;
-global using BCN_MAUI.Services;
-global using BCN_MAUI.Utilities;
-global using Fonts;
---
+++ BCN_MAUI/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace BCN_MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+         
+    }
+}
